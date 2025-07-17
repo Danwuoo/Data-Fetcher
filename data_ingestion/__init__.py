6d261f771d635152a4b8a98afe0e97b74be779cb
Danwuoo <wurrenren1006@gmail.com>
2025-07-17 08:49:48 +0800

    fix data_ingestion init module loading

--- data_ingestion/__init__.py
+++ data_ingestion/__init__.py
@@ -2,12 +2,11 @@ from __future__ import annotations
 
 import sys
 
-from backtest_data_module.data_ingestion import py as _py
-from backtest_data_module.data_ingestion import proxy as _proxy
-from backtest_data_module.data_ingestion import metrics as _metrics
-from backtest_data_module.data_ingestion import *  # noqa: F401,F403
+from backtest_data_module import data_ingestion as _di
 
-# 先註冊子模組，讓其餘模組可以順利匯入
-sys.modules[__name__ + ".py"] = _py
-sys.modules[__name__ + ".proxy"] = _proxy
-sys.modules[__name__ + ".metrics"] = _metrics
+# 先註冊子模組，避免相依模組匯入失敗
+sys.modules[__name__ + ".py"] = _di.py
+sys.modules[__name__ + ".proxy"] = _di.proxy
+sys.modules[__name__ + ".metrics"] = _di.metrics
+
+from backtest_data_module.data_ingestion import *  # noqa: F401,F403,E402
